a42e843fdc833e9b943cd78a984bd2649a4f0030
Raghav Goyal <12301raghavgoyal@gmail.com>
2022-12-03 13:55:43 +0530

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+    return (
+        <div className="">
+            <Head>
+                <title>Web3 Calender</title>
+                <meta name="description" content="" />
+                <link rel="icon" href="" />
+            </Head>
+        </div>
+    );
+}
