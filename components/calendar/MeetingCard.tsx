2d0ee88f205ac55ec7f877e1eb4f3d54685a3fa6
Sparsh Agarwal <sparshaga25@gmail.com>
2022-12-04 00:09:11 +0530

    updated file structure

---
+++ components/calendar/MeetingCard.tsx
@@ -0,0 +1,89 @@
+import { Menu, Transition } from "@headlessui/react";
+
+import { AdjustmentsVerticalIcon } from "@heroicons/react/24/outline";
+
+import { parseISO } from "date-fns";
+import { Fragment } from "react";
+import { format } from "util";
+import { classNames } from "../../utils/calendarParsers";
+
+const MeetingCard = ({ meeting }: any) => {
+  let startDateTime = parseISO(meeting.startDatetime);
+  let endDateTime = parseISO(meeting.endDatetime);
+
+  return (
+    <li className="group flex items-center space-x-4 rounded-xl px-4 py-2 focus-within:bg-lightPrimary hover:bg-lightPrimary">
+      <img
+        src={meeting.imageUrl}
+        alt=""
+        className="h-10 w-10 flex-none rounded-full"
+      />
+      <div className="flex-auto">
+        <p className="text-primary">{meeting.name}</p>
+        <p className="mt-0.5">
+          <time className="text-primary" dateTime={meeting.startDatetime}>
+            {format(startDateTime, "h:mm a")}
+          </time>{" "}
+          -{" "}
+          <time className="text-primary" dateTime={meeting.endDatetime}>
+            {format(endDateTime, "h:mm a")}
+          </time>
+        </p>
+      </div>
+      <Menu
+        as="div"
+        className="relative opacity-0 focus-within:opacity-100 group-hover:opacity-100"
+      >
+        <div>
+          <Menu.Button className="-m-2 flex items-center rounded-full p-1.5 text-primary hover:text-primary">
+            <span className="sr-only text-black">Open options</span>
+            <AdjustmentsVerticalIcon className="h-6 w-6" aria-hidden="true" />
+          </Menu.Button>
+        </div>
+
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="absolute right-0 z-10 mt-2 w-36 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <div className="py-1">
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    href="#"
+                    className={classNames([
+                      active ? "bg-primary text-white" : "text-primary",
+                      "block px-4 py-2 text-sm",
+                    ])}
+                  >
+                    Edit
+                  </a>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    href="#"
+                    className={classNames([
+                      active ? "bg-primary  text-white" : "text-primary",
+                      "block px-4 py-2 text-sm",
+                    ])}
+                  >
+                    Cancel
+                  </a>
+                )}
+              </Menu.Item>
+            </div>
+          </Menu.Items>
+        </Transition>
+      </Menu>
+    </li>
+  );
+};
+
+export default MeetingCard;
